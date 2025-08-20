cfea8fe653e694e0347a3d4016016c35cccdf463
Nanan Weng <iholdinon@gmail.com>
2025-08-20 13:40:35 +0800

    删除所有。从头开始

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import loginReducer, { topicsReducer } from "./reducers";
-
-const rootReducer = combineReducers({loginReducer, topicsReducer})
-
-export const Store = createStore(rootReducer, applyMiddleware(thunk));
